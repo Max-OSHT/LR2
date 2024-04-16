ec4cc281fd76ff5bbf34e9c5ebab35d6565ad3c1
osht <clay.00@inbox.ru>
2024-04-16 22:08:35 +0300

    First commit

---
+++ Site/front/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <div class="app">
+      <HelloWorld/>
+    </div>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
