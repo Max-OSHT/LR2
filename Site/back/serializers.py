932cf5c45c3f0706191461238dca6b96828ac4cc
osht <clay.00@inbox.ru>
2024-04-21 04:34:36 +0300

    api update

--- Site/back/serializers.py
+++ Site/back/serializers.py
@@ -3,10 +3,10 @@ from .models import *
 from .cipher import *
 
 class UserSerializer(ModelSerializer):
-    class Meta:
-        model = Users
-        fields = ["url", "username", "email", "password", "created_at"]
-        extra_kwargs = {"password": {"write_only": True}}
+        class Meta:
+                model = Users
+                fields = ["url", "username", "email", "password", "created_at"]
+                extra_kwargs = {"password": {"write_only": True}}
 
         def create(self, validated_data):
                 user = Users(username=validated_data["username"])
@@ -15,10 +15,10 @@ class UserSerializer(ModelSerializer):
                 return user
 
 class LoggSerializer(ModelSerializer):
-    class Meta:
-        model = Logg
-        fields = ["id", "log", "created_at"]
-        extra_kwargs = {"log": {"write_only": True}}
+        class Meta:
+            model = Logg
+            fields = ["id", "log", "created_at"]
+            extra_kwargs = {"log": {"write_only": True}}
         
         def create(self, validated_data):
                 log = Logg(id=validated_data["id"])
