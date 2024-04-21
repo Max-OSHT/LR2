78efbb242234551b7f85aff49426734e98d14b2a
osht <clay.00@inbox.ru>
2024-04-22 01:23:46 +0300

    full validate on the front

--- Site/front/src/views/Home.vue
+++ Site/front/src/views/Home.vue
@@ -1,65 +1,167 @@
 <script>
 import axios from 'axios';
+import { reactive } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { helpers, required, minLength, email, sameAs} from '@vuelidate/validators'
+
 export default {
-        data() {  
+        data() {
                 return {
                         mode: true,
-                        login: '',
-                        username: '',
-                        email: '',
-                        password: '',
-                        password_confirm: '',
-                        errors: []
+                        formLog: {
+                                login: '',
+                                password: '',
+                        },
+                        formReg: {
+                                username: '',
+                                email: '',
+                                password: '',
+                                password_confirm: '',
+                        }
                 }
         },
-        methods:{
-                async handleSubmit(action){
-                        if (action === true) 
-                        {
-                                const data = {
-                                        login: this.username,
-                                        login: this.email,
-                                        password: this.password
-                                };
-                                const response = await axios.post('log', data);
-                                console.log(response);
+        methods: {
+
+                async handleSubmit(action) {
+                        if (action === true) {
+                                const result = await this.v$.formLog.$validate()
+                                if (result) {
+                                        const data = {
+                                                login: this.formLog.login,
+                                                password: this.formLog.password
+                                        };
+                                        const response = await axios.post('log', data);
+                                        console.log(response);
+                                }
+                                else {
+                                        console.log("valid not successful")
+                                }
                                 return
                         }
                         else {
-                                const data = {
-                                        username: this.username,
-                                        email: this.email,
-                                        password: this.password,
-                                        password_confirm: this.password_confirm
-                                };
-                                const response = await axios.post('reg', data);
-                                console.log(response);
+                                const result = await this.v$.formReg.$validate()
+                                if (result) {
+                                        const data = {
+                                                username: this.formReg.username,
+                                                email: this.formReg.email,
+                                                password: this.formReg.password,
+                                                password_confirm: this.formReg.password_confirm
+                                        };
+                                        const response = await axios.post('reg', data);
+                                        console.log(response);
+                                }
+                                else {
+                                        console.log("valid not successful")
+                                }
                         }
-
-
                 },
-        }
+        },
+        validations() {
+                return {
+                        formLog: {
+                                login: { required, minLength: minLength(4) },
+                                password: { 
+                                        required, 
+                                        minLength: minLength(8),
+                                        containsPasswordRequirement: helpers.withMessage(
+                                                () => `The password requires an uppercase, lowercase, number and special character`,
+                                                (value) => /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.test(value)
+                                        ),
+                                },
+                        },
+                        formReg: {
+                                username: { required, minLength: minLength(4) },
+                                email: { required, email },
+                                password: { 
+                                        required,
+                                        minLength: minLength(8),
+                                        containsPasswordRequirement: helpers.withMessage(
+                                                () => `The password requires an uppercase, lowercase, number and special character`,
+                                                (value) => /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.test(value)
+                                        ),
+                                },
+                                password_confirm: { required, sameAsPassword: sameAs(this.formReg.password) }
+                        }
+                }
+        },
+        setup: () => ({ v$: useVuelidate() }),
+        // validationConfig: {
+        //         $error: true
+        // }
 }
 </script>
 
 <template>
         <div class="home">
                 <div id="title">Log Profile</div>
-                <form @submit.prevent="">   
-                        <input v-if="!mode" type="text" class="form-control" placeholder="Username" aria-label="username"
-                                aria-describedby="basic-addon1" v-model="username" required>
-                        <input type="text" class="form-control" placeholder="Login/Email" aria-label="Login"
-                                aria-describedby="basic-addon1" v-model="email" required>
-                        <input type="password" class="form-control" placeholder="Password" aria-label="Password"
-                                aria-describedby="basic-addon1" v-model="password" required>
-                        <input v-if="!mode" type="password" class="form-control" placeholder=" Confirm Password" aria-label="Password"
-                                aria-describedby="basic-addon1" v-model="password_confirm" required>
-                        <div v-if="mode" class="d-flex justify-content-between">
-                                <div><button type="submit" class="btn btn-outline-light" @click="handleSubmit(mode)">Log in</button></div>
-                                <div><button @click="mode = !mode" type="submit" class="btn btn-outline-light">No account</button></div>
+                <form @submit.prevent="">
+
+                        <!-- Log in -->
+                        <input v-if="mode" type="text" class="form-control" placeholder="Login" v-model.trim="formLog.login">
+                        <p v-if="mode && v$.formLog.login.required.$invalid" class="error">
+                                Обязательное поле
+                        </p>
+                        <p v-if="mode && v$.formLog.login.minLength.$invalid" class="error">
+                                Должно быть не менее 4 символов
+                        </p>
+
+                        <input v-if="mode" type="password" class="form-control" placeholder="Password" v-model.trim="formLog.password">
+                        <p v-if="mode && v$.formLog.password.required.$invalid" class="error">
+                                Обязательное поле
+                        </p>
+                        <p v-if="mode && v$.formLog.password.minLength.$invalid" class="error">
+                                Должно быть не менее 8 символов
+                        </p>
+                        <p v-if="mode && v$.formLog.password.containsPasswordRequirement.$invalid" class="error">
+                                Пароль должен содержать заглавные, строчные, цифры и специальные символы
+                        </p>
+
+                        <!-- Registration -->
+                        <input v-if="!mode" type="text" class="form-control" placeholder="Username" v-model.trim="formReg.username"
+                                @blur="v$.formReg.username.$touch">
+                        <div v-if="!mode && v$.formReg.username.required.$invalid" class="error">
+                                Обязательное поле
                         </div>
-                        <div v-if="!mode"><button type="submit" class="btn btn-outline-light w-100" @click="handleSubmit(mode), mode = !mode">Registration</button></div>
-                </form>  
+                        <p v-if="!mode && v$.formReg.username.minLength.$invalid" class="error">
+                                Должно быть не менее 4 символов
+                        </p>
+
+                        <input v-if="!mode" type="text" class="form-control" placeholder="Email" v-model.trim="formReg.email">
+                        <p v-if="!mode && v$.formReg.email.required.$invalid" class="error">
+                                Обязательное поле
+                        </p>
+                        <p v-if="!mode && v$.formReg.email.email.$invalid" class="error">
+                                Не корректная почта
+                        </p>
+
+                        <input v-if="!mode" type="password" class="form-control" placeholder="Password" v-model.trim="formReg.password">
+                        <p v-if="!mode && v$.formReg.password.required.$invalid" class="error">
+                                Обязательное поле
+                        </p>
+                        <p v-if="!mode && v$.formReg.password.minLength.$invalid" class="error">
+                                Должно быть не менее 8 символов
+                        </p>
+                        <p v-if="!mode && v$.formReg.password.containsPasswordRequirement.$invalid" class="error">
+                                Пароль должен содержать заглавные, строчные, цифры и специальные символы
+                        </p>
+
+                        <input v-if="!mode" type="password" class="form-control" placeholder="Confirm Password" v-model.trim="formReg.password_confirm">
+                        <p v-if="!mode && v$.formReg.password_confirm.required.$invalid" class="error">
+                                Обязательное поле
+                        </p>
+                        <p v-if="!mode && v$.formReg.password_confirm.sameAsPassword.$invalid" class="error">
+                                Пароли не совпадают
+                        </p>
+
+                        <div v-if="mode" class="d-flex justify-content-between">
+                                <div><button type="submit" class="btn btn-outline-light" @click="handleSubmit(mode)">Log
+                                                in</button></div>
+                                <div><button @click="mode = !mode" type="submit" class="btn btn-outline-light">No
+                                                account</button></div>
+                        </div>
+                        <div v-if="!mode"><button type="submit" class="btn btn-outline-light w-100"
+                                        @click="handleSubmit(mode), mode = !mode">Registration</button></div>
+                </form>
         </div>
         <div class="d-flex justify-content-center">etc. osht</div>
 </template>
@@ -72,16 +174,25 @@ export default {
         align-items: center;
         min-height: 97vh;
 }
-form > input, button{
+
+form>input,
+button {
         margin-bottom: 10px;
         border-width: 3px;
         border-color: rgb(51, 182, 123);
 }
-#title{
+
+#title {
         padding: 0;
         margin-bottom: 10px;
         font-weight: 700;
         font-size: xx-large;
         color: aliceblue;
 }
+
+.error {
+        font-size: small;
+        color: salmon;
+}
+
 </style>
